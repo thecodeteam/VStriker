09b1f52fb1bcd2796abdefdc94352f6a1dfdc3c9
Sanjeev Chauhan <schauhan@momentumsi.com>
2014-12-24 04:37:52 -0500

    Added a controller class for homepage

--- src/main/java/com/emccode/vstriker/VStriker.java
+++ src/main/java/com/emccode/vstriker/VStriker.java
@@ -3,11 +3,16 @@ package com.emccode.vstriker;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import com.emccode.vstriker.model.Account;
+import com.emccode.vstriker.view.HomepageController;
 
 /*
  * @author Sanjeev Chauhan
@@ -16,6 +21,21 @@ public class VStriker extends Application {
 	
 	private Stage primaryStage;
 	private BorderPane vStrikerLayout;
+	
+	// Replace with Account from vStrikerEntities package - ToDo
+	private ObservableList<Account> accountData = FXCollections.observableArrayList();
+	
+	// Constructor
+	public VStriker() {
+		//Adding sample data - move to vStrikerLoader package - ToDo
+		accountData.add(new Account("name1", "location1"));
+		accountData.add(new Account("name2", "location2"));
+	}
+	
+	// Return list of Accounts
+	public ObservableList<Account> getAccountData() {
+		return accountData;
+	}
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -32,7 +52,7 @@ public class VStriker extends Application {
 			System.out.println("Initializing the VStriker application");
 			//Load the layout from the fxml file
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(VStriker.class.getResource("com/emccode/vstriker/view/VStriker.fxml"));
+			//loader.setLocation(VStriker.class.getResource("com/emccode/vstriker/view/VStriker.fxml"));
 			loader.setLocation(VStriker.class.getResource("view/VStriker.fxml"));
 			vStrikerLayout = (BorderPane) loader.load();
 			
@@ -56,6 +76,10 @@ public class VStriker extends Application {
 			
 			//Show the home layout in the center of the application
 			vStrikerLayout.setCenter(homeLayout);
+			
+			// Give controller access to main app
+			HomepageController controller = loader.getController();
+			controller.setVStrikerApp(this);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
---
+++ src/main/java/com/emccode/vstriker/view/HomepageController.java
@@ -0,0 +1,40 @@
+package com.emccode.vstriker.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import com.emccode.vstriker.VStriker;
+import com.emccode.vstriker.model.Account;
+
+//@author Sanjeev Chauhan
+
+public class HomepageController {
+	@FXML
+	private TableView<Account> accountTable;
+	@FXML
+	private TableColumn<Account, String> nameColumn;
+	@FXML
+	private TableColumn<Account, String> locationColumn;
+	
+	private VStriker vStriker;
+	
+	// Contructor
+	public HomepageController() {
+	}
+	
+	// Initialize
+	@FXML
+	private void initialize() {
+		// Initialize the account table with two columns
+		nameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
+		locationColumn.setCellValueFactory(cellData -> cellData.getValue().locationProperty());
+	}
+	
+	// Set the main application
+	public void setVStrikerApp(VStriker vStriker) {
+		this.vStriker = vStriker;
+		
+		accountTable.setItems(vStriker.getAccountData());
+	}
+}
