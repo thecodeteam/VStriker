51ba879a1d236ccb4dbc04e49b0952c74f334fce
Sanjeev Chauhan <schauhan@momentumsi.com>
2015-01-05 09:46:00 -0500

    Add account to database

--- vStrikerUI/src/main/java/com/emccode/vstriker/controller/AccountController.java
+++ vStrikerUI/src/main/java/com/emccode/vstriker/controller/AccountController.java
@@ -5,8 +5,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import vStrikerBizModel.AccountBiz;
+import vStrikerEntities.Account;
 
-import  com.emccode.vstriker.VStriker;
+import com.emccode.vstriker.VStriker;
 
 //@author Sanjeev Chauhan
 
@@ -17,13 +20,19 @@ public class AccountController {
 	private Button addAPI;
 	@FXML
 	private ChoiceBox<String> chooseAPI;
-	
+	@FXML
+	private Button saveupdate;
+	@FXML
+	private TextField accountName;
+	@FXML
+	private TextField accountLocation;
+
 	private VStriker vStriker;
 
 	// Constructor
 	public AccountController() {
 	}
-	
+
 	// Set the main application
 	public void setVStrikerApp(VStriker vStriker) {
 		this.vStriker = vStriker;
@@ -33,8 +42,12 @@ public class AccountController {
 	@FXML
 	private void initialize() {
 		System.out.println("In AccountController initialize");
-		chooseAPI.setItems(FXCollections.observableArrayList("S3", "Swift", "Atmos"));
+		chooseAPI.setItems(FXCollections.observableArrayList("S3", "Swift",
+				"Atmos"));
 		chooseAPI.setValue("S3");
+		// Set prompt text
+		accountName.setPromptText("Enter Account Name");
+		accountLocation.setPromptText("Enter Account Location");
 	}
 
 	@FXML
@@ -42,12 +55,34 @@ public class AccountController {
 		System.out.println("back to Accounts button clicked");
 		vStriker.showHome();
 	}
-	
+
+	@FXML
+	public void saveupdateClicked(ActionEvent event) {
+		System.out.println("Save/Update button clicked");
+		if (accountName.getText() == null
+				|| accountName.getText().length() == 0
+				|| accountLocation.getText() == null
+				|| accountLocation.getText().length() == 0) {
+			System.out.println("Please set Account Name and Account Location");
+		} else {
+			System.out.println("Account to be added: " + accountName.getText()
+					+ " " + accountLocation.getText());
+			Account acct = new Account();
+			acct.setName(accountName.getText());
+			acct.setAccountLocation(accountLocation.getText());
+			try {
+				AccountBiz.AccountCreate(acct);
+			} catch (Exception e) {
+				System.out.println("Failed to save Account");
+				e.printStackTrace();
+			}
+		}
+	}
+
 	@FXML
 	public void addAPIClicked(ActionEvent event) {
 		System.out.println("Add API button clicked");
-		switch(chooseAPI.getValue().toString())
-		{
+		switch (chooseAPI.getValue().toString()) {
 		case "S3":
 			vStriker.showS3API();
 			break;
@@ -58,7 +93,8 @@ public class AccountController {
 			vStriker.showAtmosAPI();
 			break;
 		default:
-			System.out.println("addAPIClicked - chooseAPI is neither S3, Swift or Atmos");
+			System.out
+					.println("addAPIClicked - chooseAPI is neither S3, Swift or Atmos");
 			break;
 		}
 	}
